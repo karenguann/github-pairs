79d0d679246eb91488109a7e69ec21d01f19c0c9
Karen <kar3n.guan@gmail.com>
2023-07-14 10:36:57 -0400

    Initial Commit

---
+++ github pairs/github_pairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_pairsApp.swift
+//  github pairs
+//
+//  Created by Karen Guan on 2023-07-14.
+//
+
+import SwiftUI
+
+@main
+struct github_pairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
